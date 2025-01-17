17feaae05937e3de890b22a2d9753f144719b21a
davier enrique perez cabrera <davierperez11.2@gmail.com>
2025-01-16 22:49:21 -0500

    feat: creating bean of use case in configuration

---
+++ src/main/java/com/prueba/productInventoryManagement/Configuration.java
@@ -0,0 +1,15 @@
+package com.prueba.productInventoryManagement;
+
+import com.prueba.productInventoryManagement.application.CategoryUseCase;
+import com.prueba.productInventoryManagement.application.ProductUseCase;
+import org.springframework.context.annotation.Bean;
+
+@org.springframework.context.annotation.Configuration
+public class Configuration {
+
+    @Bean
+    public ProductUseCase productBean() { return new ProductUseCase(); }
+
+    @Bean
+    public CategoryUseCase categoryBean() { return new CategoryUseCase(); }
+}
