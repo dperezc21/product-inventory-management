021d972faf58ba8c89e7f218445f00a12d45633e
davier enrique perez cabrera <davierperez11.2@gmail.com>
2025-01-16 20:59:55 -0500

    feat: initial commit

---
+++ src/main/java/com/prueba/productInventoryManagement/ProductInventoryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.productInventoryManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductInventoryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductInventoryManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prueba/productInventoryManagement/ProductInventoryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.productInventoryManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductInventoryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
